6792905cdd1af11dd1f7e189b7d5e681acd7af01
William Roe <git@wjlr.org.uk>
2018-10-15 18:20:07 +0100

    Page 13
    Example world+projectile virtual cannon thing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+extern crate ray_tracer_challenge;
+
+use ray_tracer_challenge::tuples::Tuple;
+
+struct Projectile {
+    position: Tuple,
+    velocity: Tuple,
+}
+
+struct World {
+    gravity: Tuple,
+    wind: Tuple,
+}
+
+impl World {
+    fn tick(&self, p: Projectile) -> Projectile {
+        let position = p.position + p.velocity;
+        let velocity = p.velocity + self.gravity + self.wind;
+        Projectile { position, velocity }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut p = Projectile {
+        position: Tuple::point(0.0, 1.0, 0.0),
+        velocity: Tuple::vector(1.0, 5.0, 0.0).normalize(),
+    };
+    let w = World {
+        gravity: Tuple::vector(0.0, -0.1, 0.0),
+        wind: Tuple::vector(-0.01, 0.0, 0.0),
+    };
+    let mut num_ticks = 0;
+    while p.position.y > 0.0 {
+        num_ticks += 1;
+        println!(
+            "Tick {:4}. Projectile is at ({:.3}, {:.3}, {:.3})...",
+            num_ticks, p.position.x, p.position.y, p.position.z
+        );
+        let new_p = w.tick(p);
+        p = new_p;
+    }
 }
