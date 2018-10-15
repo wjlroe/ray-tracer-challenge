f3d4e130cab4f385e2204d3fbfcbc0142a2c845a
William Roe <git@wjlr.org.uk>
2018-10-15 08:37:02 +0100

    Page 6
    Subtracting tuples

--- src/tuples.rs
+++ src/tuples.rs
@@ -43,6 +43,19 @@ impl ops::Add for Tuple {
     }
 }
 
+impl ops::Sub for Tuple {
+    type Output = Tuple;
+
+    fn sub(self, other: Tuple) -> Tuple {
+        Tuple {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+            w: self.w - other.w,
+        }
+    }
+}
+
 #[test]
 fn test_w_of_1_is_a_point() {
     let a = Tuple::new(4.3, -4.2, 3.1, 1.0);
@@ -82,4 +95,25 @@ fn test_adding_two_tuples() {
     let a1 = Tuple::new(3.0, -2.0, 5.0, 1.0);
     let a2 = Tuple::new(-2.0, 3.0, 1.0, 0.0);
     assert_eq!(a1 + a2, Tuple::new(1.0, 1.0, 6.0, 1.0));
+}
+
+#[test]
+fn test_subtracting_two_points() {
+    let p1 = Tuple::point(3.0, 2.0, 1.0);
+    let p2 = Tuple::point(5.0, 6.0, 7.0);
+    assert_eq!(p1 - p2, Tuple::vector(-2.0, -4.0, -6.0));
+}
+
+#[test]
+fn test_subtracting_a_vector_from_a_point() {
+    let p = Tuple::point(3.0, 2.0, 1.0);
+    let v = Tuple::vector(5.0, 6.0, 7.0);
+    assert_eq!(p - v, Tuple::point(-2.0, -4.0, -6.0));
+}
+
+#[test]
+fn test_subtracting_two_vectors() {
+    let v1 = Tuple::vector(3.0, 2.0, 1.0);
+    let v2 = Tuple::vector(5.0, 6.0, 7.0);
+    assert_eq!(v1 - v2, Tuple::vector(-2.0, -4.0, -6.0));
 }
