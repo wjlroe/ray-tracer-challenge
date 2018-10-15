2e219307c46b58447dae2da9183ce06136a85985
William Roe <git@wjlr.org.uk>
2018-10-15 08:23:25 +0100

    Page 4
    Added basic vector and point factory methods

---
+++ src/tuples.rs
@@ -0,0 +1,63 @@
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Tuple {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
+}
+
+impl Tuple {
+    pub fn new(x: f32, y: f32, z: f32, w: f32) -> Self {
+        Tuple { x, y, z, w }
+    }
+
+    pub fn point(x: f32, y: f32, z: f32) -> Self {
+        Tuple::new(x, y, z, 1.0)
+    }
+
+    pub fn vector(x: f32, y: f32, z: f32) -> Self {
+        Tuple::new(x, y, z, 0.0)
+    }
+
+    pub fn is_point(&self) -> bool {
+        self.w == 1.0
+    }
+
+    pub fn is_vector(&self) -> bool {
+        self.w == 0.0
+    }
+}
+
+#[test]
+fn test_w_of_1_is_a_point() {
+    let a = Tuple::new(4.3, -4.2, 3.1, 1.0);
+    assert_eq!(a.x, 4.3);
+    assert_eq!(a.y, -4.2);
+    assert_eq!(a.z, 3.1);
+    assert_eq!(a.w, 1.0);
+    assert!(a.is_point());
+    assert!(!a.is_vector());
+}
+
+#[test]
+fn test_w_of_0_is_a_vector() {
+    let a = Tuple::new(4.3, -4.2, 3.1, 0.0);
+    assert_eq!(a.x, 4.3);
+    assert_eq!(a.y, -4.2);
+    assert_eq!(a.z, 3.1);
+    assert_eq!(a.w, 0.0);
+    assert!(!a.is_point());
+    assert!(a.is_vector());
+}
+
+#[test]
+fn test_point_factory() {
+    let p = Tuple::point(4.0, -4.0, 3.0);
+    assert_eq!(p, Tuple::new(4.0, -4.0, 3.0, 1.0));
+}
+
+#[test]
+fn test_vector_factory() {
+    let v = Tuple::vector(4.0, -4.0, 3.0);
+    assert_eq!(v, Tuple::new(4.0, -4.0, 3.0, 0.0));
+}
