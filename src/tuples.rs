13829d595ebabfe2113ce14476b53557527eb365
William Roe <git@wjlr.org.uk>
2018-10-15 08:28:56 +0100

    Page 5
    Adding tuples

--- src/tuples.rs
+++ src/tuples.rs
@@ -1,3 +1,5 @@
+use std::ops;
+
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Tuple {
     pub x: f32,
@@ -25,6 +27,19 @@ impl Tuple {
 
     pub fn is_vector(&self) -> bool {
         self.w == 0.0
+    }
+}
+
+impl ops::Add for Tuple {
+    type Output = Tuple;
+
+    fn add(self, other: Tuple) -> Tuple {
+        Tuple {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: self.w + other.w,
+        }
     }
 }
 
@@ -60,4 +75,11 @@ fn test_point_factory() {
 fn test_vector_factory() {
     let v = Tuple::vector(4.0, -4.0, 3.0);
     assert_eq!(v, Tuple::new(4.0, -4.0, 3.0, 0.0));
+}
+
+#[test]
+fn test_adding_two_tuples() {
+    let a1 = Tuple::new(3.0, -2.0, 5.0, 1.0);
+    let a2 = Tuple::new(-2.0, 3.0, 1.0, 0.0);
+    assert_eq!(a1 + a2, Tuple::new(1.0, 1.0, 6.0, 1.0));
 }
