d3adf242ae6ac9b5b18d554b93c547a41733da6b
William Roe <git@wjlr.org.uk>
2018-10-23 19:12:44 +0100

    Page 65
    Ray intersect with sphere

--- src/rays.rs
+++ src/rays.rs
@@ -1,5 +1,7 @@
 use tuples::Tuple;
 
+pub struct Sphere {}
+
 pub struct Ray {
     pub origin: Tuple,
     pub direction: Tuple,
@@ -12,6 +14,25 @@ impl Ray {
 
     pub fn position(&self, t: f32) -> Tuple {
         self.origin + self.direction * t
+    }
+
+    pub fn intersect(&self, _sphere: Sphere) -> Vec<f32> {
+        let sphere_to_ray = self.origin - Tuple::point(0.0, 0.0, 0.0);
+        let a = self.direction.dot(self.direction);
+        let b = 2.0 * self.direction.dot(sphere_to_ray);
+        let c = sphere_to_ray.dot(sphere_to_ray) - 1.0;
+        let discriminant = b * b - 4.0 * a * c;
+        if discriminant < 0.0 {
+            vec![]
+        } else {
+            let t1 = (-b - discriminant.sqrt()) / (2.0 * a);
+            let t2 = (-b + discriminant.sqrt()) / (2.0 * a);
+            if t1 > t2 {
+                vec![t2, t1]
+            } else {
+                vec![t1, t2]
+            }
+        }
     }
 }
 
@@ -31,4 +52,55 @@ fn test_computing_a_point_from_a_distance() {
     assert_eq!(r.position(1.0), Tuple::point(3.0, 3.0, 4.0));
     assert_eq!(r.position(-1.0), Tuple::point(1.0, 3.0, 4.0));
     assert_eq!(r.position(2.5), Tuple::point(4.5, 3.0, 4.0));
+}
+
+#[test]
+fn test_a_ray_intersects_a_sphere_at_two_points() {
+    let r =
+        Ray::new(Tuple::point(0.0, 0.0, -5.0), Tuple::vector(0.0, 0.0, 1.0));
+    let s = Sphere {};
+    let xs = r.intersect(s);
+    assert_eq!(xs.len(), 2);
+    assert_eq!(xs[0], 4.0);
+    assert_eq!(xs[1], 6.0);
+}
+
+#[test]
+fn test_a_ray_intersects_a_sphere_at_a_tangent() {
+    let r =
+        Ray::new(Tuple::point(0.0, 1.0, -5.0), Tuple::vector(0.0, 0.0, 1.0));
+    let s = Sphere {};
+    let xs = r.intersect(s);
+    assert_eq!(xs.len(), 2);
+    assert_eq!(xs[0], 5.0);
+    assert_eq!(xs[1], 5.0);
+}
+
+#[test]
+fn test_a_ray_misses_a_sphere() {
+    let r =
+        Ray::new(Tuple::point(0.0, 2.0, -5.0), Tuple::vector(0.0, 0.0, 1.0));
+    let s = Sphere {};
+    let xs = r.intersect(s);
+    assert_eq!(xs.len(), 0);
+}
+
+#[test]
+fn test_a_ray_originates_inside_a_sphere() {
+    let r = Ray::new(Tuple::point(0.0, 0.0, 0.0), Tuple::vector(0.0, 0.0, 1.0));
+    let s = Sphere {};
+    let xs = r.intersect(s);
+    assert_eq!(xs.len(), 2);
+    assert_eq!(xs[0], -1.0);
+    assert_eq!(xs[1], 1.0);
+}
+
+#[test]
+fn test_a_sphere_is_behind_a_ray() {
+    let r = Ray::new(Tuple::point(0.0, 0.0, 5.0), Tuple::vector(0.0, 0.0, 1.0));
+    let s = Sphere {};
+    let xs = r.intersect(s);
+    assert_eq!(xs.len(), 2);
+    assert_eq!(xs[0], -6.0);
+    assert_eq!(xs[1], -4.0);
 }
